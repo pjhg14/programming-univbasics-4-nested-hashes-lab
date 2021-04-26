c59d0c9e3b1d250e62aa2e0ca0aa99388e93452b
pj <pjhg14@gmail.com>
2021-04-26 09:38:47 -0400

    commit tree 53228b2facf95077acea6a53ec462391d0683923

--- nested.rb
+++ nested.rb
@@ -16,6 +16,7 @@ def hopper
       }
     }
 
+  programmer_hash[:grace_hopper]
 end
 
 def alan_kay_is_known_for
@@ -37,6 +38,7 @@ def alan_kay_is_known_for
       }
     }
 
+  programmer_hash[:alan_kay][:known_for]
 end
 
 def dennis_ritchies_language
@@ -56,6 +58,8 @@ def dennis_ritchies_language
       }
     }
 
+
+  programmer_hash[:dennis_ritchie][:languages][0]
 end
 
 def adding_matz
@@ -82,6 +86,13 @@ def adding_matz
       }
     }
 
+
+  programmer_hash[:yukihiro_matsumoto] = {
+    known_for: "Ruby",
+    languages: ["LISP", "C"]
+  }
+
+  programmer_hash
 end
 
 def changing_alan
@@ -104,6 +115,9 @@ def changing_alan
       }
     }
 
+  programmer_hash[:alan_kay][:known_for] = "GUI"
+
+  programmer_hash
 end
 
 def adding_to_dennis
@@ -126,4 +140,8 @@ def adding_to_dennis
       }
     }
 
+
+  programmer_hash[:dennis_ritchie][:languages] << "Assembly"
+
+  programmer_hash
 end
